411fcdeebe0ec0a85510284b9ed1497a0f41feac
nicolasnna <nicolas.norambuenaa@gmail.com>
2024-09-30 21:58:08 -0300

    Change config route

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import {
-  BrowserRouter as Router
-} from 'react-router-dom'
-import store from '@reducer/store.js'
-import { ThemeProvider } from '@mui/material'
-import theme from './theme/theme.js'
-import './style.sass'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import App from "./App.jsx"
+import { Provider } from "react-redux"
+import { BrowserRouter as Router } from "react-router-dom"
+import store from "@reducer/store.js"
+import { ThemeProvider } from "@mui/material"
+import theme from "./theme/theme.js"
+import "./style.sass"
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const basename = import.meta.env.MODE === "production" ? "/study-streak" : "/"
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <Router>
+      <Router basename={basename}>
         <Provider store={store}>
           <App />
         </Provider>
       </Router>
     </ThemeProvider>
-  </React.StrictMode>,
+  </React.StrictMode>
 )
